99da688f6153d692e9067d2b9cb6fc8567c640b2
Evelina <evelinaavegas@gmail.com>
2026-06-18 20:19:21 +0300

    create main-content structure. debugging

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header  from './Header/Header.jsx'
+import Content from './Content/Content.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Content />
   </StrictMode>,
 )
