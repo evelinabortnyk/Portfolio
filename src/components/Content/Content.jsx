a01d3384845fc89884cf216234242418d57f910e
Evelina <evelinaavegas@gmail.com>
2026-08-12 17:12:51 +0300

    feat: update about section

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -11,14 +11,22 @@ function Content () {
         <Navigation />
         <Header />
         <main>
-            
-            <div className="main-conteiner" id='about-me'>
+            <div className="main-conteiner about-me" id='about-me'>
                 <h2>About me</h2>
                 <p>
                     Hi, I'm Evelina, a Frontend Developer passionate about creating modern, responsive, and user-friendly web applications.
                     I work with HTML, CSS, JavaScript, React, and modern web development tools. I enjoy turning ideas into clean, functional interfaces and continuously improving my skills by building personal projects and learning new technologies.
                     I have commercial experience as a Frontend Developer and completed professional training in web development with distinction. Currently, I am looking for opportunities to contribute to real-world projects, grow as a developer, and collaborate with experienced teams.
                     When I'm not coding, I enjoy sports, reading, and exploring new ideas in technology.
+                </p>
+                <h4>What I bring:</h4>
+                <p>
+                    I’m disciplined, persistent and genuinely passionate about what I do. 
+                    When I’m working on something I enjoy, I can easily lose track of time.
+                    <br/>
+                    I approach challenges step by step, breaking bigger goals into clear and manageable tasks. 
+                    I learn quickly, adapt to new situations and like looking at problems from different perspectives — 
+                    often finding solutions or opportunities that aren’t immediately obvious.
                 </p>
             </div>
             <div className='main-conteiner' id='my-scills'>
