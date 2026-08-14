22c8d806fcfef43aec3f11a95c5d9d8ef6f9ff39
Evelina <evelinaavegas@gmail.com>
2026-08-14 18:03:27 +0300

    refactor: add useEfect for menu button

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,61 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './navigation.css'
 import Logo from '../Header/img/logo.png'
 
 
 function Navigation() {
-    const [menuActive, setMenuActive] = useState(false)
-    const [classListBtn, setClassListBtn] = useState("menu-btn")
-    const [classListMenu, setClassListMenu] = useState("burger-menu")
+  const [menuActive, setMenuActive] = useState(false)
+  const [classListBtn, setClassListBtn] = useState("menu-btn")
+  const [classListMenu, setClassListMenu] = useState("burger-menu")
 
-    const naviArr = [
-        {'value': 'About me', 'link' : 'about-me',},
-        {'value': 'My skills', 'link' : 'my-skills',},
-        {'value': 'Portfolio', 'link' : 'portfolio',},
-        {'value': 'Contacts', 'link' : 'contacts',},
-    ]
+  const naviArr = [
+    { 'value': 'About me', 'link': 'about-me', },
+    { 'value': 'My skills', 'link': 'my-skills', },
+    { 'value': 'Portfolio', 'link': 'portfolio', },
+    { 'value': 'Contacts', 'link': 'contacts', },
+  ]
+  useEffect(() => {
+    if (menuActive === true) {
+      setClassListBtn('menu-btn-active')
+      setClassListMenu('burger-menu-active')
+    } else {
+      setClassListBtn('menu-btn')
+      setClassListMenu('burger-menu')
+    }
+  }, [menuActive])
 
-    function menuClick() {
-        setMenuActive(!menuActive)
-    
-        if (menuActive === true) {
-          setClassListBtn('menu-btn-active')
-          setClassListMenu('burger-menu-active')
-        } else {
-          setClassListBtn('menu-btn')
-          setClassListMenu('burger-menu')
-        }
-      }
 
-      function scrollToSection (id){
-        document.getElementById(id)?.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start',
-        });
-      };
+  function scrollToSection(id) {
+    document.getElementById(id)?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
 
-    return (
-        <>
-            <div className='header-navigation'>
-            <div className='logo-container'>
-                <img src={Logo} alt="logo" className='logo'/>
-            </div>
-                <button href="#" className={`menu-btn ${classListBtn}`} value={menuActive} onClick={() => menuClick()}>
-                    <span className="menu-btn-burger"></span>
-                </button>
-            </div>
-            <div className="menu">
-                <nav className={`burger-menu ${classListMenu}`}>
-                    <ul className="menu-list">
-                        {naviArr.map((item, index) => (
-                            <li key={index} onClick={() => scrollToSection(item.link)}>{item.value}</li>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
-      </>
-    )
+  return (
+    <>
+      <div className='header-navigation'>
+        <div className='logo-container'>
+          <img src={Logo} alt="logo" className='logo' />
+        </div>
+        <button href="#" className={`menu-btn ${classListBtn}`} value={menuActive} onClick={() => setMenuActive(!menuActive)}>
+          <span className="menu-btn-burger"></span>
+        </button>
+      </div>
+      <div className="menu">
+        <nav className={`burger-menu ${classListMenu}`}>
+          <ul className="menu-list">
+            {naviArr.map((item, index) => (
+              <li key={index} onClick={() => scrollToSection(item.link)}>{item.value}</li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
 }
 
 export default Navigation
