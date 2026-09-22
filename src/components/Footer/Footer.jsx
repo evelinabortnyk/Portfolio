b3402562bda69cfdb906156bab2f7c5d9f0d138b
Evelina Bortnyk <evelinaavegas@gmail.com>
2026-09-22 09:41:18 +0300

    fix: correcting email link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import githubIcon from './img/github.svg'
 
 function Footer (){
     const contactsArr = [
-        {'src': mailIcon, 'link': 'mailto:bortnykevelinaa@gmail.com', 'type': 'Email',},
+        {'src': mailIcon, 'link': 'https://mail.google.com/mail/?view=cm&fs=1&to=bortnykevelinaa@gmail.com', 'type': 'Email',},
         {'src': linkedinIcon, 'link': 'https://www.linkedin.com/in/evelina-bortnyk-81bb83416/', 'type': 'Linkedin',},
         {'src': githubIcon, 'link': 'https://github.com/evelinabortnyk', 'type': 'GitHub',},
     ]
